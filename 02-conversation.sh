8dad2634590d0bb1e93eca47402e7b0eb76cf4c4
chamalasya <lasyareddyc@gmail.com>
2025-05-18 12:33:51 +0800

    hello world started

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "trump:: hey musk, how are you?"
+echo "musk:: hello trump, i am fine. how are you doing"
+echo "trump:: i am fine too. what's up?"
+echo "musk:: nothing, just going to mars now, will you come"
+echo "trmup:: sorry, you carry on! i will come once you come back"
