800699d55914a211d31c05d02761d043f78a4143
chamalasya <lasyareddyc@gmail.com>
2025-05-22 17:19:40 +0800

    special vars

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -2,3 +2,10 @@
 
 echo "All varabiles passed to the script $@"
 echo "number of varaibles $#"
+echo "script name $0"
+echo "current directory $PWD"
+echo "user running the script $USER"
+echo "home directory of the user $HOME"
+echo "PID of the script $$"
+sleep 10 &
+echo "PID of last command in background $!
