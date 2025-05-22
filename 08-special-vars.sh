996aaf3f5c93235801a615ec7bf68ee655a6a881
chamalasya <lasyareddyc@gmail.com>
2025-05-22 17:26:32 +0800

    special vars

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -7,5 +7,5 @@ echo "current directory $PWD"
 echo "user running the script $USER"
 echo "home directory of the user $HOME"
 echo "PID of the script $$"
-sleep 10 &
+sleep 100 &
 echo "PID of last command in background $!
